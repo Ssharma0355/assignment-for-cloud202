9d7e2206e3e69098d4976969b75d278669d311f2
sachin_sde <ssachin.s@myclnq.co.in>
2025-03-01 17:27:26 +0530

    all-tab-ui-done

--- pages/security.tsx
+++ pages/security.tsx
@@ -1,5 +1,38 @@
+import { useRouter } from "next/router";
 const Security: React.FC = () => {
-  return <h1 className="text-2xl">Security Overview Page</h1>;
+    const router = useRouter();
+  return (
+    <div className="p-4 border border-green-800 rounded-lg ">
+      {/* Form Section */}
+      <form className="space-y-4">
+        {/* Header */}
+        <div className="bg-[#3C7069] text-white p-3 rounded-t-lg">
+          <h1 className="text-lg font-bold">Security</h1>
+        </div>
+        <div className="flex flex-col">
+          <label className="font-medium">Security Name</label>
+          <input type="text" className="border p-2 rounded" />
+        </div>
+        <div className="flex justify-between mt-4">
+          <button
+            type="button"
+            className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition"
+            onClick={() => router.push("/rag")}
+          >
+            Previous
+          </button>
+
+          <button
+            type="button"
+            className={"bg-[#3C7069] px-4 py-2 rounded text-white "}
+            onClick={() => router.push("/security")}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Security;
